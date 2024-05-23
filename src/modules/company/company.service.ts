a263ff17759b18ce62608655ed3f7281f2ff8a72
luansantiago0 <luansantiago0200@gmail.com>
2024-05-23 17:09:11 -0300

    feat: add route of create company

--- src/modules/company/company.service.ts
+++ src/modules/company/company.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDTO } from './dto/CreatyCompany.dto';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class CompanyService {
-  create(createCompanyDto: CreateCompanyDTO) {
-    return 'This action adds a new company';
-  }
+  constructor(private readonly prismaService: PrismaService) {}
 
+  async create(data: CreateCompanyDTO) {
+    try {
+      const company = await this.prismaService.company.create({
+        data,
+      });
+      return company;
+    } catch (error) {
+      throw `Erro ao criar uma empresa: ${error}`;
+    }
+  }
 }
