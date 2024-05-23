eb8cb5a926561d659dbfadbe2b8bdf9f06e0bbbf
luansantiago0 <luansantiago0200@gmail.com>
2024-05-22 21:15:31 -0300

    feat: add route create event

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDTO } from './dto/CreateEvent.dto';
+import { PrismaService } from 'src/db/prisma.service';
 
 @Injectable()
 export class EventService {
@@ -7,4 +8,28 @@ export class EventService {
     return 'This action adds a new event';
   }
 
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createEvent({
+    name,
+    companyId,
+    categoryId,
+    description,
+    imageUrl,
+    price,
+    discount,
+  }: CreateEventDTO) {
+    const event = await this.prismaService.event.create({
+      data: {
+        name,
+        companyId,
+        categoryId,
+        description,
+        imageUrl,
+        price,
+        discount,
+      },
+    });
+    return event;
+  }
 }
